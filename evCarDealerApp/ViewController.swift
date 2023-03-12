33d9554ef4bcb0ae2e865264e1f842fc631d0aec
Ben Ko <designerko1215@gmail.com>
2023-03-12 19:01:19 +0800

    Initial Commit

---
+++ evCarDealerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evCarDealerApp
+//
+//  Created by Wei Chiang Ko on 2023/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
